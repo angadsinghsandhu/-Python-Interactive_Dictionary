fd067450dbe923d6d1d3f6428d946090e40aa56d
angadsinghsandhu <angadsandhuworkmail@gmail.com>
2020-08-12 18:13:47 +0530

    updated readme and comments

--- Interactive Dictionary.py
+++ Interactive Dictionary.py
@@ -1,35 +1,41 @@
-#Project 1
-
+# Imports
 import json
 from difflib import SequenceMatcher
 from difflib import get_close_matches
 
-# r = SequenceMatcher(None, "rainn", "rain").ratio()
-
-fname = 'D:/Code/Python/Projects/01 Interactive Dictionary/076 data.json'
+# importing dictionary in .json format
+fname = './data.json'
 data = open(fname).read()
 data_j = json.loads(data)
 
+# helper functions
+
+
 def Meaning(word):
     return data_j[word]
+
 
 def output(word):
     arr = Meaning(word)
     for i in arr:
         print(i)
 
+
+# taking in input
 word = input('Enter Word : ')
 word = word.lower()
 
-try :
+# outputting word if found else, error handling
+try:
     output(word)
-except :
+except:
     arr = get_close_matches(word, data_j.keys(), n=1, cutoff=0.7)
 
     if arr == []:
         print('the word does not exist')
     else:
-        deci = input('Did you mean ({}), if yes then type (y) or else type (n) : '.format(arr[0]))
+        deci = input(
+            'Did you mean ({}), if yes then type (y) or else type (n) : '.format(arr[0]))
         if deci == 'y':
             output(arr[0])
         else:
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# -Python-Interactive_Dictionary
-A python project that outputs the meaning of a word like a dictionary
+# <ins> -Python-Interactive_Dictionary </ins>
+
+## <ins> Description </ins>
+A python project that outputs the meaning of a word. Hence, automating a dictionary.
+It also has advanced checking system to look for errors. Eg: If you type `Rainn` 
+instead of `Rain`. It will prompt you for it, instead of simply throwing an error.
+
+---
+
+## Project Details
+* <ins> Name </ins> :  Interactive_Dictionary
+* <ins> Category </ins> :  Python
+
+---
+
+### Packages Used ###
+| name    | PyPi Links   |
+| ------- | ------------ |
+| json    | **in-built** |
+| difflib | **in-built** |
+
+
